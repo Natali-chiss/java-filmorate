988893f80340c0d14950e375aed38f739b54062d
Chistyakova Natalya <nata.chistyakova.03@yandex.ru>
2025-04-16 15:14:18 +0300

    Исправлены ошибки чекстайла

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserServiceImpl.java
@@ -17,7 +17,7 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService {
     private final UserRepositoryInterface userRepository;
     private final UserMapper mapper;
-    private final String USER_NOT_FOUND = "Не найден пользователь с id =";
+    private final String userNotFound = "Не найден пользователь с id =";
 
     @Override
     public UserDto saveUser(User user) {
@@ -28,7 +28,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDto updateUser(User user) {
         userRepository.getUserById(user.getId())
-                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND + user.getId()));
+                .orElseThrow(() -> new NotFoundException(userNotFound + user.getId()));
         User updatedUser = userRepository.updateUser(user);
         return mapper.mapToUserDto(updatedUser);
     }
@@ -36,27 +36,27 @@ public class UserServiceImpl implements UserService {
     @Override
     public void addFriend(Long userId, Long friendId) {
         userRepository.getUserById(userId)
-                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND + userId));
+                .orElseThrow(() -> new NotFoundException(userNotFound + userId));
         userRepository.getUserById(friendId)
-                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND + friendId));
+                .orElseThrow(() -> new NotFoundException(userNotFound + friendId));
         userRepository.addFriend(userId, friendId);
     }
 
     @Override
     public void deleteFriend(Long userId, Long friendId) {
         userRepository.getUserById(userId)
-                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND + userId));
+                .orElseThrow(() -> new NotFoundException(userNotFound + userId));
         userRepository.getUserById(friendId)
-                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND + friendId));
+                .orElseThrow(() -> new NotFoundException(userNotFound + friendId));
         userRepository.deleteFriend(userId, friendId);
     }
 
     @Override
     public List<UserDto> getCommonFriends(Long user1Id, Long user2Id) {
         userRepository.getUserById(user1Id)
-                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND + user1Id));
+                .orElseThrow(() -> new NotFoundException(userNotFound + user1Id));
         userRepository.getUserById(user2Id)
-                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND + user2Id));
+                .orElseThrow(() -> new NotFoundException(userNotFound + user2Id));
         if (user1Id.equals(user2Id)) {
             throw new ConditionsNotMetException("Пользователи должны иметь разные id");
         }
@@ -68,7 +68,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<UserDto> getFriendsList(Long userId) {
         userRepository.getUserById(userId)
-                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND + userId));
+                .orElseThrow(() -> new NotFoundException(userNotFound + userId));
         return userRepository.getFriendsList(userId).stream()
                 .map(mapper::mapToUserDto)
                 .collect(Collectors.toList());
